f7a1f6cbb9a4ac47246b41701ed5ae04c86fa60c
UgurcanSARICI <ugurcansarici@gmail.com>
2022-10-14 21:42:25 +0300

    card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import { languages } from "./helper/data";
 import "./App.css";
 import reactsvg from "./assets/react.svg";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div className="bos-div">
       {/*  */}
       <img src={reactsvg} alt="reactlogo" className="reactlogo" />
+      <div className="down-container">
+        <div>
+          <h2>Language</h2>
+          <div className="main-container">
+            {languages.map((language, index) => {
+              return (
+                <div className="container">
+                  <Card key={index} {...language} />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
       {/*  */}
     </div>
   );
